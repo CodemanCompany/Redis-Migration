f11cb86bc261bac819267ff5a588405c51d495f8
Gustavo Martinez <gustavo@codeman.company>
2022-08-21 21:57:58 -0500

    Progress Linux

--- main.py
+++ main.py
@@ -13,8 +13,29 @@
 
 import redis
 
-r = redis.Redis(
-    host="localhost",
-    port=6379, 
-    password=""
+# Variables
+data = []
+
+# r = redis.Redis(
+# 	host = "localhost",
+# 	port = 6379, 
+# 	password = ""
+# )
+r = redis.StrictRedis(
+	host = "localhost",
+	port = 6379, 
+	db = 0
 )
+
+for key in r.scan_iter():
+	data.append( {
+		"key": key,
+		"value": r.get( key ),
+	} )
+
+
+print( data )
+
+# r.set( "test", "Gus" )
+# value = r.get( "test" )
+# print( value )
